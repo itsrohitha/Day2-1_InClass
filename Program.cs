6b99d905d8b6f0ade45805457ddc835b466d391e
Naga Rohitha Nannapaneni <nagarohitha@usf.edu>
2019-09-05 16:09:40 -0400

    changes

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Day2_1_InClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter your age: ");
+                int age = int.Parse(Console.ReadLine());
+
+                if (age >= 21)
+                {
+                    Console.WriteLine("You're old enough to drink alcohol");
+                }
+                else
+                {
+                    Console.WriteLine("You're not old enough to drink!!!");
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("You have an error");
+            }
+        }
+    }
+}
